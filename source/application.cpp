2c515f28c419a08c482e45e9be5b767ae68164c2
gusjasponde <gustavocalonico@gmail.com>
2020-07-22 00:36:43 -0300

    Update CMakeLists for dependencies

--- source/application.cpp
+++ source/application.cpp
@@ -1,11 +1,31 @@
 #include <application.h>
+#include <SDL.h>
+#include <stdio.h>
 
 Application::Application(const Config &config) : m_config(config){
 
 }
 
 void Application::run_loop() {
-
+  SDL_Window* window = NULL;
+  SDL_Surface* screenSurface = NULL;
+  if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+    fprintf(stderr, "could not initialize sdl2: %s\n", SDL_GetError());
+    return;
+  }
+  window = SDL_CreateWindow("hello_sdl2", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
+                            this->m_config.windowX,this->m_config.windowY, SDL_WINDOW_SHOWN);
+  if (window == NULL) {
+    fprintf(stderr, "could not create window: %s\n", SDL_GetError());
+    return;
+  }
+  screenSurface = SDL_GetWindowSurface(window);
+  SDL_FillRect(screenSurface, NULL, SDL_MapRGB(screenSurface->format, 0xFF, 0xFF, 0xFF));
+  SDL_UpdateWindowSurface(window);
+  SDL_Delay(2000);
+  SDL_DestroyWindow(window);
+  SDL_Quit();
+  return;
 }
 
 Config Application::get_config() {
