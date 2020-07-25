7cfec29dee0b0f9608ceb23ef009139f02cc6911
gusjasponde <gustavocalonico@gmail.com>
2020-07-24 22:22:15 -0300

    Add master renderer

---
+++ source/render/masterrenderer.cpp
@@ -0,0 +1,16 @@
+#include <render/masterrenderer.h>
+
+void Render::MasterRenderer::finish_render(SDL_Window &window) {
+}
+
+void Render::MasterRenderer::draw_characters(){
+}
+
+void Render::MasterRenderer::draw_hud(){
+}
+
+void Render::MasterRenderer::draw_points(){
+}
+
+void Render::MasterRenderer::draw_walls(){
+}
---
+++ test/source/render/masterrenderer.cpp
@@ -0,0 +1,7 @@
+#include <doctest/doctest.h>
+#include <render/masterrenderer.h>
+
+using namespace Render;
+
+TEST_CASE("Master renderer should be built correctly") {
+}
