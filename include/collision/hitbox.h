3b324c922d227ae24410ab6b179d62bfaa329eb3
gusjasponde <gustavocalonico@gmail.com>
2020-07-30 20:49:51 -0300

    Enhance collision and entity

--- include/collision/hitbox.h
+++ include/collision/hitbox.h
@@ -2,7 +2,7 @@
 
 namespace Collision {
   struct Hitbox {
-    Hitbox(const glm::vec2& dim, glm::vec2& pos) : dim(dim), pos(pos){};
+    Hitbox(const glm::vec2& dim, const glm::vec2& pos) : dim(dim), pos(pos){};
     void update(glm::vec2 pos);
     bool collide(Hitbox& other) const;
 
--- include/entity.h
+++ include/entity.h
@@ -4,9 +4,9 @@
 #include <glm/vec2.hpp>
 
 struct Entity {
-  Entity(const glm::vec2 &pos, const glm::vec2 &vel) : position(pos), velocity(vel) {}
+  Entity(const glm::vec2& dim, const glm::vec2& pos, const glm::vec2& vel)
+      : hitbox(dim, pos), velocity(vel) {}
 
-  glm::vec2 position;
-  glm::vec2 velocity;
   Collision::Hitbox hitbox;
+  glm::vec2 velocity;
 };
