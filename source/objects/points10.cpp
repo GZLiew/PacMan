8c4f32bc68eadfc6bdb719ac453fcf571f6cc6d5
gusjasponde <gustavocalonico@gmail.com>
2020-08-08 18:44:27 -0300

    Fix hitbox vs entity sizing

--- source/objects/points10.cpp
+++ source/objects/points10.cpp
@@ -1,7 +1,7 @@
 #include <objects/points10.h>
 
 Objects::Points10::Points10(const glm::vec2& pos, std::shared_ptr<Render::MasterRenderer> renderer)
-    : Entity({16.0f, 16.0f}, pos, {0.f, 0.f}, Collision::Hitbox({16.0f, 16.0f}, pos)),
+    : Entity({16.0f, 16.0f}, pos, {0.f, 0.f}, Collision::Hitbox({8.0f, 8.0f}, pos)),
       m_renderer(renderer) {
   this->m_texture = std::unique_ptr<SDL_Texture, SDL_TextureDeleter>(
       Utils::loadSDLTexture(renderer->renderer().get(), "resources/score/10pts.bmp"));
@@ -12,8 +12,8 @@ void Objects::Points10::eat() { this->eaten = true; }
 void Objects::Points10::draw() {
   if (this->eaten) return;
 
-  SDL_Rect renderQuad{(int)this->hitbox.pos.x, (int)this->hitbox.pos.y, (int)this->hitbox.dim.x,
-                      (int)this->hitbox.dim.y};
+  SDL_Rect renderQuad{(int)this->position.x, (int)this->position.y, (int)this->dimension.x,
+                      (int)this->dimension.y};
   SDL_RenderCopyEx(this->m_renderer->renderer().get(), this->m_texture.get(), NULL, &renderQuad, 0,
                    NULL, SDL_FLIP_NONE);
 }
--- source/objects/wall.cpp
+++ source/objects/wall.cpp
@@ -71,8 +71,8 @@ Objects::Wall::Wall(Type t, const glm::vec2& pos, std::shared_ptr<Render::Master
 }
 
 void Objects::Wall::draw() {
-  SDL_Rect renderQuad{(int)this->hitbox.pos.x, (int)this->hitbox.pos.y, (int)this->hitbox.dim.x,
-                      (int)this->hitbox.dim.y};
+  SDL_Rect renderQuad{(int)this->position.x, (int)this->position.y, (int)this->dimension.x,
+                      (int)this->dimension.y};
   SDL_RenderCopyEx(this->m_renderer->renderer().get(), this->m_texture.get(), NULL,
                     &renderQuad, this->m_degree, NULL, this->m_flip_type);
 }
